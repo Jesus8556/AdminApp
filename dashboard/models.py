65960d68edfb290584ab29a3e793c2606328a65d
jesus8556 <luis.gutierrez.m@tecsup.edu.pe>
2023-05-26 23:54:02 -0500

    Dashboard añadido

--- dashboard/models.py
+++ dashboard/models.py
@@ -43,10 +43,33 @@ class Ciclo(models.Model):
     carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
     numero = models.IntegerField()
 
+class Aula(models.Model):
+    nombre = models.CharField(max_length=100)
+    cursos = models.ManyToManyField('Curso')
+
+
+
 class Curso(models.Model):
     nombre = models.CharField(max_length=100)
     ciclo = models.ForeignKey(Ciclo, on_delete=models.CASCADE)
     alumnos = models.ManyToManyField('Alumno')
     profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    aulas = models.ManyToManyField('Aula')
+    def crear_notas_laboratorio(self):
+        for numero in range(1, 17):
+            NotaLaboratorio.objects.create(curso=self, numero=numero)
 
+class NotaLaboratorio(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    numero = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+
+class Horario(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    dia = models.DateField()
+    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
+
+    def __str__(self):
+        return f"Horario - Curso: {self.curso.nombre}, Día: {self.dia}, Hora inicio: {self.hora_inicio}, Hora fin: {self.hora_fin}"
 
